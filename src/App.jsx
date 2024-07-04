5e32fd9180278b6a3a0fdbb4360cb9aac8907564
Falashree Shirodkar <falashreeshirodkar07@gmail.com>
2024-07-05 03:19:17 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,29 +2,37 @@ import { useState, useEffect } from 'react'
 import './App.css'
 import { useDispatch } from 'react-redux';
 import AuthService from './appwrite/auth';
-import {login,logout} from './store/authSlice'
+import { login, logout } from './store/authSlice'
+import { Footer, Header } from './components';
+import { Outlet } from 'react-router-dom';
 
 function App() {
   const [loading, setLoading] = useState(true);
   const dispatch = useDispatch()
 
-  useEffect(()=>{
+  useEffect(() => {
     AuthService.getCurrentUser()
-    .then((userData)=>{
-      if(userData){
-        dispatch(login({userData}))
-      }else{
-        dispatch(logout())
-      }
-    })
-    .finally()
-  },[])
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }))
+        } else {
+          dispatch(logout())
+        }
+      })
+      .finally(() => setLoading(false))
+  }, [])
 
-  return (
-    <>
-      <h1>BHLOG</h1>
-    </>
-  )
+  return !loading ? (
+<div className='min-h-screen flex flex-wrap content-between justify-center items-center bg-gray-400'>
+<div className='w-1/2 block text-center'>
+        <Header />
+        <main>
+          TODO:  <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
 
-export default App
+export default App;
