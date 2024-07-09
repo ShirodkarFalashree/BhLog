5cc70787440abdc5f2660ce2dbd482e790401d6c
Falashree Shirodkar <falashreeshirodkar07@gmail.com>
2024-07-09 06:14:50 +0530

    changes

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,12 @@
 import Header from "./header/Header";
 import Footer from "./footer/Footer";
 import Container from "./container/Container";
-
+import Logo from "./Logo";
+import LogoutBtn from './header/LogoutBtn'
 export {
     Header,
     Footer,
-    Container
+    Container,
+    Logo,
+    LogoutBtn
 }
