5be2cc30f6e431d2b037604f6aa980fa1de62e23
Falashree Shirodkar <falashreeshirodkar07@gmail.com>
2024-07-13 11:00:30 +0530

    change

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -3,8 +3,15 @@ import appwriteService from "../appwrite/config"
 import { Link } from 'react-router-dom'
 function PostCard({$id,title,featuredImage}) {
   return (
-   <Link></Link>
+   <Link to={`/post/${$id}`}>
+    <div className="w-full bg-gray-100 rounded-xl p-4">
+      <div className="w-full justify-center mb-4">
+        <img src={appwriteService.getFilePreview} alt="" />
+      </div>
+      <h2>{title}</h2>
+    </div>
+   </Link>
   )
 }
 
-export default PostCard
+export default PostCard;
